bbbcb0bd77ecb6d6a5ff87a5af2c417d1669e82c
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2016-01-13 12:48:14 +0530

    Kubernetes service nat script, Remove subscription information from .pangaea, Better bootstrapping by retrying ssh

--- pangaea/azure/create-nat-rule.sh
+++ pangaea/azure/create-nat-rule.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 if [ ! "$#" = "3" ]; then
     echo "USAGE: $0 <frontent-port> <backend-port> <name-for-nat-rule>"
-    exit
+    exit 1
 fi
 
 SCRIPT_DIR=$(dirname $0)
@@ -22,6 +24,8 @@ fi
 azure network lb inbound-nat-rule create \
     --resource-group "$AZURE_NAME" --name "$AZURE_NAME-nat-$3" --lb-name "$AZURE_NAME-lb" \
     --protocol tcp --frontend-port "$1" --backend-port "$2"
+
+# Note: --name here is the NIC name, not a resource name
 azure network nic inbound-nat-rule add \
     --resource-group "$AZURE_NAME" --name "$AZURE_NAME-nic" --lb-name "$AZURE_NAME-lb" \
     --inbound-nat-rule-name "$AZURE_NAME-nat-$3"
--- pangaea/azure/up.sh
+++ pangaea/azure/up.sh
@@ -19,6 +19,7 @@ fi
 CLOUD_CONFIG=$ROOT_DIR/pangaea/kubernetes/cloud-config.sh
 
 CREATED_JSON=$ROOT_DIR/.tmp/azure_public_ip.json
+CREATED_BIP_JSON=$ROOT_DIR/.tmp/azure_bip.json
 
 KEYS_PATH=$ROOT_DIR/pangaea/pki/keys/$AZURE_NAME
 KEYS_FILE=$KEYS_PATH/login_key
@@ -31,6 +32,8 @@ fi
 
 # PROVISION AZURE RESOURCES
 
+# TODO: use template based deployments instead
+
 azure group create -n "$AZURE_NAME" -l "$AZURE_LOCATION"
 
 azure network vnet create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-vnet" --location "$AZURE_LOCATION" --address-prefixes "10.240.0.0/24"
@@ -38,12 +41,12 @@ azure network vnet subnet create --resource-group "$AZURE_NAME" --name "$AZURE_N
 azure network public-ip create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-pubip" --location "$AZURE_LOCATION" --allocation-method Static --domain-name-label "$AZURE_NAME-hasura"
 azure network lb create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-lb" --location "$AZURE_LOCATION"
 azure network lb frontend-ip create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-fip" --lb-name "$AZURE_NAME-lb" --public-ip-name "$AZURE_NAME-pubip"
-azure network lb address-pool create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-bip" --lb-name "$AZURE_NAME-lb"
+azure network lb address-pool create --resource-group "$AZURE_NAME" --name "$AZURE_NAME-bip" --lb-name "$AZURE_NAME-lb" --json | tee "$CREATED_BIP_JSON"
 
 azure network nic create \
     --resource-group "$AZURE_NAME" --name "$AZURE_NAME-nic" --location "$AZURE_LOCATION" \
     --subnet-name "$AZURE_NAME-subnet" --subnet-vnet-name "$AZURE_NAME-vnet" \
-    --lb-address-pool-ids "/subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$AZURE_NAME/providers/Microsoft.Network/loadBalancers/$AZURE_NAME-lb/backendAddressPools/$AZURE_NAME-bip"
+    --lb-address-pool-ids "$(cat "$CREATED_BIP_JSON" | jq -r '.id')"
 
 azure vm create \
     --resource-group "$AZURE_NAME" \
@@ -69,13 +72,24 @@ NAT_SCRIPT=$ROOT_DIR/pangaea/azure/create-nat-rule.sh
 # BOOTSTRAP KUBERNETES
 
 azure network public-ip show "$AZURE_NAME" "$AZURE_NAME-pubip" --json > "$CREATED_JSON"
+NODE_IP=$(cat "$CREATED_JSON" | jq -r '.ipAddress')
 
 function init_ssl_and_setup_archive {
-    local NODE_IP=$(cat "$CREATED_JSON" | jq -r '.ipAddress')
 
     local SETUP_TAR=$ROOT_DIR/.tmp/setup.tar # written to by init script
     local SETUP_MD5=$ROOT_DIR/.tmp/setup.md5
     "$ROOT_DIR/pangaea/setup/init_ssl_and_setup_archive.sh" create $AZURE_NAME $NODE_IP
+
+    local SSH_RETRIES=0
+
+    while ! ssh -i "$KEYS_FILE" core@$NODE_IP date &>/dev/null; do
+        if [ $SSH_RETRIES -gt 5 ]; then
+            echo "SSH retries failed. Resources have been provisioned under group $AZURE_NAME"
+            exit 1
+        fi
+        sleep 5
+        SSH_RETRIES=$((SSH_RETRIES+1))
+    done
 
     scp -i "$KEYS_FILE" "$SETUP_TAR" core@$NODE_IP:/tmp/setup.tar
     scp -i "$KEYS_FILE" "$SETUP_MD5" core@$NODE_IP:/tmp/setup.md5
